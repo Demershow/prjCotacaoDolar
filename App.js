ee134790f4606a85a4428a7d68a6e423b73e6b42
demershow <demersontorres520@gmail.com>
2022-06-13 09:46:37 -0300

    App Finalizado

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import Cotacao from './Components/Cotacao';
+import API from './Components/API';
 
 export default function App() {
+
+    const [dolar, setDolar] = useState("");
+
+    async function precoDolar(){
+      const resposta = await API.get('json/last/USD-BRL');
+      setDolar(resposta.data.USDBRL);
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity
+          style={styles.bloco}
+          onPress={precoDolar}>
+        <Text style={styles.txtBloco}>DÓLAR PARA REAL</Text>
+      </TouchableOpacity>
+      <Text>Clique no botão para consultar os dados da cotação.</Text>
+      <Cotacao data={dolar}/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    margin: 0, padding: 0,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  txtBloco: {
+    borderWidth: 3,
+    borderColor: '#000',
+    backgroundColor: '#34eb37',
+    borderRadius: 30,
+    padding: 15,
   },
 });
---
+++ Components/Cotacao.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text} from 'react-native';
+
+
+export default function Cotacao(props) {
+  return (
+  <>
+    <Text style={styles.resposta}>Compra: {props.data.bid}</Text>
+    <Text style={styles.resposta}>Venda:  {props.data.ask}</Text>
+    <Text style={styles.resposta}>Variação: {props.data.varBid}</Text>
+    <Text style={styles.resposta}>Máximo: {props.data.high}</Text>
+    <Text style={styles.resposta}>Mínimo: {props.data.low}</Text>
+  </>
+  );
+}
+
+const styles = StyleSheet.create({
+  resposta: {
+        fontsize:15,
+        marginTop:10,
+    }
+})
